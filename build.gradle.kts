d9e097a8e54f5f963eb1bdb9af82ef46652f7e84
Bryce Groff <bgroff@hawaii.edu>
2026-01-21 10:15:46 -0800

    add the web components to the jandex.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
 
 allprojects {
     group = "com.revethq.iam"
-    version = "0.1.5"
+    version = "0.1.6"
 
     repositories {
         mavenCentral()
--- scim/build.gradle.kts
+++ scim/build.gradle.kts
@@ -1,5 +1,14 @@
+import org.jboss.jandex.IndexWriter
+import org.jboss.jandex.Indexer
+
 plugins {
     kotlin("jvm")
+}
+
+buildscript {
+    dependencies {
+        classpath("io.smallrye:jandex:3.2.0")
+    }
 }
 
 dependencies {
@@ -32,4 +41,35 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation("io.mockk:mockk:1.13.8")
     testRuntimeOnly("org.jboss.resteasy:resteasy-core:6.2.7.Final")
+}
+
+tasks.register("jandex") {
+    description = "Generate Jandex index"
+    group = "build"
+
+    dependsOn(tasks.named("classes"))
+
+    doLast {
+        val indexer = Indexer()
+        val classesDir = layout.buildDirectory.dir("classes/kotlin/main").get().asFile
+
+        classesDir.walkTopDown()
+            .filter { it.isFile && it.extension == "class" }
+            .forEach { classFile ->
+                classFile.inputStream().use { indexer.index(it) }
+            }
+
+        val index = indexer.complete()
+        val metaInfDir = layout.buildDirectory.dir("resources/main/META-INF").get().asFile
+        metaInfDir.mkdirs()
+
+        val jandexFile = File(metaInfDir, "jandex.idx")
+        jandexFile.outputStream().use { IndexWriter(it).write(index) }
+
+        println("Generated Jandex index with ${index.knownClasses.size} classes")
+    }
+}
+
+tasks.named("jar") {
+    dependsOn("jandex")
 }
